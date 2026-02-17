d781abddeeac182508b2a8015b8af12dc7ebe61a
aprudakov <alexrudakov5@gmail.com>
2026-02-17 22:26:29 +0000

    Init

---
+++ src/Vault.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IERC20 {
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+contract Vault {
+    address public safe;
+    address public leader;
+    IERC20 public asset;
+
+    event Deposited(address indexed user, uint256 amount);
+    event VaultInitialized(address indexed safe, address indexed leader, address indexed asset);
+
+    constructor(address _safe, address _leader, address _asset) {
+        safe = _safe;
+        leader = _leader;
+        asset = IERC20(_asset);
+
+        emit VaultInitialized(_safe, _leader, _asset);
+    }
+
+    function deposit(uint256 amount) external {
+        require(asset.transferFrom(msg.sender, safe, amount), "TRANSFER_FAILED");
+        emit Deposited(msg.sender, amount);
+    }
+}
