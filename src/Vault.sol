00715f5b88dade4cda9c274f6bf38a0536e163c8
aprudakov <alexrudakov5@gmail.com>
2026-02-18 15:38:25 +0000

    install safe

--- src/Vault.sol
+++ src/Vault.sol
@@ -2,7 +2,11 @@
 pragma solidity ^0.8.13;
 
 interface IERC20 {
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
 }
 
 contract Vault {
@@ -11,7 +15,11 @@ contract Vault {
     IERC20 public asset;
 
     event Deposited(address indexed user, uint256 amount);
-    event VaultInitialized(address indexed safe, address indexed leader, address indexed asset);
+    event VaultInitialized(
+        address indexed safe,
+        address indexed leader,
+        address indexed asset
+    );
 
     constructor(address _safe, address _leader, address _asset) {
         safe = _safe;
@@ -22,7 +30,10 @@ contract Vault {
     }
 
     function deposit(uint256 amount) external {
-        require(asset.transferFrom(msg.sender, safe, amount), "TRANSFER_FAILED");
+        require(
+            asset.transferFrom(msg.sender, safe, amount),
+            "TRANSFER_FAILED"
+        );
         emit Deposited(msg.sender, amount);
     }
 }
